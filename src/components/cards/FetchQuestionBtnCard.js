9664ea4c7ef2fec02c773c7285d05f41497204ab
Raphael Matile <raphael.matile@gmail.com>
2018-03-12 09:31:09 +0100

    provotum/backend#22: Fetch ballot contract address as long as none is returned. Closes provotum/backend#22

--- src/components/cards/FetchQuestionBtnCard.js
+++ src/components/cards/FetchQuestionBtnCard.js
@@ -1,6 +1,9 @@
 import React from "react";
 import {Button, Card, Col, Form, Icon, Input, Row} from "antd";
 import PropTypes from "prop-types";
+import logger from "react-logger";
+import axios from "axios/index";
+import UpdatedTextInput from "../UpdatedTextInput";
 
 class FetchQuestionBtnCard extends React.Component {
   constructor(props) {
@@ -8,6 +11,28 @@ class FetchQuestionBtnCard extends React.Component {
 
     this.props.form.validateFields();
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      ballotContractAddress: ''
+    };
+
+    let fetchAddressIntervalId = setInterval(() => {
+      axios.get("/ballot/address").then(response => {
+        logger.log('Retrieved ballot contract address');
+
+        if (!(response.data.hasOwnProperty('address')) || null === response.data.address) {
+          logger.error('Response does not contain ballot contract address or is null');
+
+          return;
+        }
+
+        clearInterval(fetchAddressIntervalId);
+
+        this.setState({
+          ballotContractAddress: response.data.address
+        });
+      }).catch(error => logger.error('Failed to retrieve ballot contract address: ' + error));
+    }, 2000);
   }
 
   handleSubmit(e) {
@@ -15,14 +40,18 @@ class FetchQuestionBtnCard extends React.Component {
 
     this.props.form.validateFields((err) => {
       if (!err) {
-        this.props.actions.onClickHandler(this.props.form.getFieldsValue());
+        let ballotContractAddress = document.getElementById('ballot-contract-address').value;
+
+        if (! this.props.validators.addressValidator(ballotContractAddress)) {
+          return;
+        }
+
+        this.props.actions.onClickHandler({'address': ballotContractAddress});
       }
     });
   }
 
   render() {
-    const {getFieldDecorator, getFieldsError} = this.props.form;
-
     let isButtonDisabled = ! this.props.isConnected;
     let isInputDisabled = !this.props.isConnected;
 
@@ -32,33 +61,14 @@ class FetchQuestionBtnCard extends React.Component {
         <Form onSubmit={this.handleSubmit}>
           <Row>
             <Form.Item>
-              {getFieldDecorator('address', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please enter the ballot contract address',
-                    validator: (rule, value, callback) => {
-                      // avoid firing this validator if the value is not present
-                      if (undefined === value) {
-                        callback(false);
-                      } else {
-                        if (this.props.validators.addressValidator(value)) {
-                          // no argument -> validation is ok
-                          callback();
-                        } else {
-                          callback(false);
-                        }
-                      }
-                    }
-                  }
-                ]
-              })(
-                <Input
+                <UpdatedTextInput
+                  id={'ballot-contract-address'}
                   disabled={isInputDisabled}
+                  value={this.state.ballotContractAddress}
+                  type={'text'}
                   prefix={<Icon type="question-circle-o" style={{color: 'rgba(0,0,0,.25)'}}/>}
                   placeholder="Ballot Contract Address"
                 />
-              )}
             </Form.Item>
           </Row>
           <Row>
